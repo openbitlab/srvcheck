22fa29bd37df806f887d6abdbb5848487adeb6a9
Lu191 <gigiamendolagine.la@gmail.com>
2022-03-19 12:58:37 +0100

    Fix and improvements

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
 rpc_substrate_port=9933
 rpc_cosmos_port=26657
 
+check_docker () {
+    check=$(echo $1 | grep docker)
+    if [ ! -z "$check" ]
+    then
+	echo "It's in a docker container"
+    fi
+}
+
 rpc_substrate=$(lsof -i:$rpc_substrate_port)
 #check if we're dealing with a substrate based blockchain
 if [ ! -z "$rpc_substrate" ]
 then 
     echo "It's a substrate based blockchain"
-    check=$(echo $rpc_substrate | grep docker)
-    if [ ! -z "$check" ]
-    then
-	echo "It's in a docker container"
-    fi
+    check_docker "$rpc_substrate"
 fi
 
 rpc_cosmos=$(lsof -i:$rpc_cosmos_port)
@@ -18,9 +22,5 @@ rpc_cosmos=$(lsof -i:$rpc_cosmos_port)
 if [ ! -z "$rpc_cosmos" ]
 then 
     echo "It's a cosmos based blockchain"
-    check=$(echo $rpc_cosmos | grep docker)
-    if [ ! -z "$check" ]
-    then
-	echo "It's in a docker container"
-    fi
+    check_docker "$rpc_cosmos"
 fi
