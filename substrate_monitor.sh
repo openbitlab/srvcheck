53703a02c9806d0ad58392bb9565f789e5cd211f
Lu191 <gigiamendolagine.la@gmail.com>
2022-03-28 09:31:42 +0200

    Added checks for github new release on substrate monitor

--- substrate_monitor.sh
+++ substrate_monitor.sh
@@ -31,4 +31,21 @@ while true; do
     else
         echo "$name Signaloff: OK"
     fi
+
+    #check new version
+    if [[ ! -z $git_api && ! -z $local_version ]]
+    then
+        git_version=$(curl -s -H 'Content-Type: application/json' $git_api |  jq -r ".tag_name")
+
+        v1=$(echo $git_version | sed 's/[^0-9]*//g')
+        v2=$(echo $local_version | sed 's/[^0-9]*//g')
+        echo $v1 - $v2
+	if [[ $v2 -ge $v1 ]]
+        then
+            echo "$name Signaloff: OK"
+        else
+            echo "$name Signaloff: FAIL"
+            curl -s -X POST https://api.telegram.org/bot$api_token/sendMessage -d text="$name has new release: $git_version" -d chat_id=$chat_id
+        fi
+    fi
 done
