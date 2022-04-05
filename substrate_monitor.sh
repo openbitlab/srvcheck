c9f6ea167cb7e4bb2fe32bf4e2831be43eeda24a
franzops <francesco.lobascio@gmail.com>
2022-04-05 17:01:53 +0200

    Added tg msg for node in sync

--- substrate_monitor.sh
+++ substrate_monitor.sh
@@ -12,6 +12,7 @@ stuck_e=$'\342\233\224'
 rel_e=$'\360\237\222\277'
 peers_down_e=$'\360\237\206\230'
 sync_e=$'\342\235\227'
+sync_ok_e=$'\342\243\205'
 
 curl -s -X POST https://api.telegram.org/bot$api_token/sendMessage -d text="$name monitor started $start_e" -d chat_id=$chat_id
 
@@ -43,6 +44,10 @@ while true; do
 
     else
         echo "$name Signaloff: OK"
+        if [[ $is_stuck -eq 1 ]]
+        then 
+            curl -s -X POST https://api.telegram.org/bot$api_token/sendMessage -d text="$name is now in sync $sync_ok_e" -d chat_id=$chat_id
+        fi
         i_sync=$sync_mod
         is_stuck=0
     fi
