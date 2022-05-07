a6bd70990fade39130849cc20706277331958f85
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-05-07 10:21:04 +0200

    task cpu fix

--- srvcheck/tasks/tasksystemcpualert.py
+++ srvcheck/tasks/tasksystemcpualert.py
@@ -11,6 +11,6 @@ class TaskSystemCpuAlert(Task):
 		usage = self.system.getUsage()
 
 		if usage.cpuUsage > 90:
-			return self.notify('CPU usage is above %d%%' % usage['cpu'])
+			return self.notify('CPU usage is above %d%%' % usage.cpu)
 
 		return False
