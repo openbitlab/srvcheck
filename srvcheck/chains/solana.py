5a25c2b8379220303155984e2ca1762ba1d01ac4
Lu191 <gigiamendolagine.la@gmail.com>
2022-05-12 15:54:31 +0200

    Added basic rpc methods for solana monitoring

---
+++ srvcheck/chains/solana.py
@@ -0,0 +1,49 @@
+from ..notification import Emoji
+from .chain import Chain
+from ..tasks import Task,  hours
+import requests
+
+class Solana (Chain):
+	TYPE = "solana"
+	NAME = ""
+	BLOCKTIME = 60
+	EP = "http://localhost:8899/"
+	TASKS = []
+	
+	def __init__(self, conf):
+		super().__init__(conf)
+
+	def detect(conf):
+		try:
+			Solana(conf).getVersion()
+			return True
+		except:
+			return False
+
+	def getHealth(self):
+		return self.rpcCall('getHealth')
+
+	def getLatestVersion(self):
+		c = requests.get('https://api.github.com/repos/' + self.conf['chain']['ghRepository']+ '/releases/latest').json()
+		return c['tag_name']
+
+	def getVersion(self):
+		return self.rpcCall('getVersion')["solana-core"]
+
+	def getHeight(self):
+		return self.rpcCall('getBlockHeight')
+
+	def getBlockHash(self):
+		return self.rpcCall('getLatestBlockhash')['value']['blockhash']
+
+	def getPeerCount(self):
+		raise Exception('Abstract getPeerCount()')
+
+	def getNetwork(self):
+		raise Exception('Abstract getNetwork()')
+
+	def isStaking(self):
+		raise Exception('Abstract isStaking()')
+
+	def isSynching(self):
+		raise Exception('Abstract isSynching()')
