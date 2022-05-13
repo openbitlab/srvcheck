7526366ef655f9ce20e051dfd8c67989318433ce
AlessandroPaparella <61472307+AlessandroPaparella@users.noreply.github.com>
2022-05-13 11:40:20 +0200

    fix install with tag & get version hard coded

--- srvcheck/tasks/taskautoupdater.py
+++ srvcheck/tasks/taskautoupdater.py
@@ -1,18 +1,19 @@
+from distutils.core import setup
 from . import Task, minutes, hours
 from ..utils import Bash
 import requests
 import subprocess
-import sys
+from ....srvcheck import srvcheck
 
 class TaskAutoUpdater(Task):
     def __init__(self, conf, notification, system, chain):
         super().__init__('TaskAutoUpdater', conf, notification, system, chain, minutes(60), hours(2))
     
     def run(self):
-        nTag = requests.get('https://api.github.com/repos/openbitlab/srvcheck/git/refs/tags').json()[-1]['ref'].split('/')[-1]
-        if self.conf["version"]["tagVersion"] != nTag:
+        nTag = requests.get('https://api.github.com/repos/openbitlab/srvcheck/git/refs/tags').json()[-1]['ref'].split('/')[-1].split('v')[1]
+        if srvcheck.__version__ != nTag:
             self.notify('Installing new monitor version: %s' % (nTag))
-            Bash('pip install --upgrade git+https://github.com/openbitlab/srvcheck')
+            Bash('pip install --upgrade git+https://github.com/openbitlab/srvcheck@'+nTag)
             Bash('systemctl restart node-monitor.service')
 
       
