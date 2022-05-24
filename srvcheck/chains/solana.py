ab63f7d844ac3d7c7fd98d2545de1148dc864160
Lu191 <gigiamendolagine.la@gmail.com>
2022-05-24 13:21:15 +0200

    Fix output

--- srvcheck/chains/solana.py
+++ srvcheck/chains/solana.py
@@ -126,7 +126,7 @@ class TaskSolanaSkippedSlots(Task):
 		bp_info = self.chain.getBlockProduction()
 		skipped_perc = (bp_info[0] - bp_info[1]) / bp_info[0]
 		if skipped_perc > self.THRESHOLD_SKIPPED_SLOT:
-			return self.notify('skipped %d%% of assigned slots (%d/%d) %s' % (int(skipped_perc), bp_info[1], bp_info[0], Emoji.BlockMiss))
+			return self.notify('skipped %d%% of assigned slots (%d/%d) %s' % (int(skipped_perc) * 100, bp_info[1], bp_info[0], Emoji.BlockMiss))
 		return False
 
 class Solana (Chain):
