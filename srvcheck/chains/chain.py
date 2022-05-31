44926e04ced139d841fd661f1f96627744c419b7
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-05-31 11:38:16 +0200

    fix linting

--- srvcheck/chains/chain.py
+++ srvcheck/chains/chain.py
@@ -9,69 +9,69 @@ ConfSet.addItem(ConfItem('chain.service', None, str, 'systemd service name'))
 ConfSet.addItem(ConfItem('chain.localVersion', None, str, 'local version'))
 
 def rpcCall(url, method, params=[]):
-    d = requests.post(url, json={'jsonrpc': '2.0', 'id': 1, 'method': method, 'params': params}).json()
-    return d['result']
+	d = requests.post(url, json={'jsonrpc': '2.0', 'id': 1, 'method': method, 'params': params}).json()
+	return d['result']
 
 
 def getCall(url, data):
-    return requests.get(url, json=data).json()
+	return requests.get(url, json=data).json()
 
 
 class Chain:
-    NAME = ""
-    BLOCKTIME = 10
+	NAME = ""
+	BLOCKTIME = 10
 
-    def __init__(self, conf):
-        self.conf = conf
-        ConfSet.setDefaultValue('chain.endpoint', self.EP)
-        ConfSet.setDefaultValue('chain.name', self.NAME)
+	def __init__(self, conf):
+		self.conf = conf
+		ConfSet.setDefaultValue('chain.endpoint', self.EP)
+		ConfSet.setDefaultValue('chain.name', self.NAME)
 
-    def rpcCall(self, method, params=[]):
-        """ Calls the RPC method with the given parameters """
-        return rpcCall(self.EP, method, params)
+	def rpcCall(self, method, params=[]):
+		""" Calls the RPC method with the given parameters """
+		return rpcCall(self.EP, method, params)
 
-    def getCall(self, r, data=None):
-        """ Calls the GET method with the given parameters """
-        return getCall(self.EP + r, data)
+	def getCall(self, r, data=None):
+		""" Calls the GET method with the given parameters """
+		return getCall(self.EP + r, data)
 
-    ### Abstract methods
-    @staticmethod
-    def detect(conf):
-        """ Checks if the current server is running this chain """
-        raise Exception('Abstract detect()')
+	### Abstract methods
+	@staticmethod
+	def detect(conf):
+		""" Checks if the current server is running this chain """
+		raise Exception('Abstract detect()')
 
-    def getVersion(self):
-        """ Returns software version """
-        raise Exception('Abstract getVersion()')
+	def getVersion(self):
+		""" Returns software version """
+		raise Exception('Abstract getVersion()')
 
-    def getLatestVersion(self):
-        """ Returns software local version """
-        gh_repo = self.conf.getOrDefault('chain.ghRepository')
-        if gh_repo:
-            c = requests.get(f"https://api.github.com/repos/{gh_repo}/releases/latest").json()
-            return c['tag_name']
-        raise Exception('No github repo specified!')
+	def getLatestVersion(self):
+		""" Returns software local version """
+		gh_repo = self.conf.getOrDefault('chain.ghRepository')
+		if gh_repo:
+			c = requests.get(f"https://api.github.com/repos/{gh_repo}/releases/latest").json()
+			return c['tag_name']
+		raise Exception('No github repo specified!')
 
-    def getHeight(self):
-        """ Returns the block height """
-        raise Exception('Abstract getHeight()')
+	def getHeight(self):
+		""" Returns the block height """
+		raise Exception('Abstract getHeight()')
 
-    def getBlockHash(self):
-        """ Returns the block height """
-        raise Exception('Abstract getHeight()')
+	def getBlockHash(self):
+		""" Returns the block height """
+		raise Exception('Abstract getHeight()')
 
-    def getPeerCount(self):
-        """ Returns the number of peers """
-        raise Exception('Abstract getPeerCount()')
+	def getPeerCount(self):
+		""" Returns the number of peers """
+		raise Exception('Abstract getPeerCount()')
 
-    def getNetwork(self):
-        """ Returns network used """
-        raise Exception('Abstract getNetwork()')
+	def getNetwork(self):
+		""" Returns network used """
+		raise Exception('Abstract getNetwork()')
 
-    def isStaking(self):
-        """ Returns true if the node is staking """
-        raise Exception('Abstract isStaking()')
+	def isStaking(self):
+		""" Returns true if the node is staking """
+		raise Exception('Abstract isStaking()')
 
-    def isSynching(self):
-        """ Returns true if the node is synching """
-        raise Exception('Abstract isSynching()')
+	def isSynching(self):
+		""" Returns true if the node is synching """
+		raise Exception('Abstract isSynching()')
--- srvcheck/chains/tendermint.py
+++ srvcheck/chains/tendermint.py
@@ -1,12 +1,11 @@
-from ..notification import Emoji
-from .chain import Chain
-from ..tasks import Task,  hours, minutes
-import requests
-from ..utils import Bash
-from ..utils import ConfItem, ConfSet
 import json
 import configparser
 import re
+from ..notification import Emoji
+from .chain import Chain
+from ..tasks import Task,  hours, minutes
+from ..utils import Bash
+from ..utils import ConfItem, ConfSet
 
 ConfSet.addItem(ConfItem('chain.activeSet', description='active set of validators'))
 ConfSet.addItem(ConfItem('chain.blockWindow', 100, int))
@@ -16,7 +15,7 @@ class TaskTendermintBlockMissed(Task):
 	def __init__(self, conf, notification, system, chain, checkEvery=minutes(1), notifyEvery=minutes(5)):
 		super().__init__('TaskTendermintBlockMissed',
 		      conf, notification, system, chain, checkEvery, notifyEvery)
-			  
+
 		self.BLOCK_WINDOW = self.conf.getOrDefault('chain.blockWindow')
 		self.THRESHOLD_NOTSIGNED = self.conf.getOrDefault('chain.thresholdNotsigned')
 		self.prev = None
@@ -28,16 +27,18 @@ class TaskTendermintBlockMissed(Task):
 	def run(self):
 		nblockh = self.chain.getHeight()
 
-		if self.prev == None:
+		if self.prev is None:
 			self.prev = nblockh
 		missed = 0
 		start = nblockh - self.BLOCK_WINDOW
 		while start < nblockh:
-				if not next((x for x in self.chain.getSignatures(start) if x['validator_address'] == self.chain.getValidatorAddress()), None): missed += 1
-				start += 1
+			if not next((x for x in self.chain.getSignatures(start) if x['validator_address'] == self.chain.getValidatorAddress()), None): 
+				missed += 1
+			start += 1
+
 		if missed >= self.THRESHOLD_NOTSIGNED and self.prev != nblockh:
 			self.prev = nblockh
-			return self.notify('%d not signed blocks in the latest %d %s' % (missed, self.BLOCK_WINDOW, Emoji.BlockMiss))
+			return self.notify(f'{missed} not signed blocks in the latest {self.BLOCK_WINDOW} {Emoji.BlockMiss}')
 
 		return False
 
@@ -50,14 +51,14 @@ class TaskTendermintNewProposal(Task):
 	@staticmethod
 	def isPluggable(conf):
 		return True
-	
+
 	def run(self):
 		nProposal=self.chain.getLatestProposal()
 		if not self.prev:
 			self.prev = self.chain.getLatestProposal()
 		elif self.prev["proposal_id"] != nProposal["proposal_id"]:
 			self.prev = nProposal
-			return self.notify(' got new proposal: %s %s' % (nProposal["content"]["title"], Emoji.Proposal))
+			return self.notify(f' got new proposal: {nProposal["content"]["title"]} {Emoji.Proposal}')
 
 		return False
 
@@ -81,27 +82,28 @@ class TaskTendermintPositionChanged(Task):
 		if npos != self.prev:
 			prev = self.prev
 			self.prev = npos
+
 			if npos > prev:
-				return self.notify('position decreased from %d to %d %s' % (prev, npos, Emoji.PosDown))
+				return self.notify(f'position decreased from {prev} to {npos} {Emoji.PosDown}')
 			else:
-				return self.notify('position increased from %d to %d %s' % (prev, npos, Emoji.PosUp))
-				
+				return self.notify(f'position increased from {prev} to {npos} {Emoji.PosUp}')
+
 		return False
 
 	def getValidatorPosition(self):
 		bh = str(self.chain.getHeight())
 		active_vals = []
-		if (self.ACTIVE_SET is None):
+		if self.ACTIVE_SET is None:
 			active_s = int(self.chain.rpcCall('validators', [bh, "1", "1"])['total'])
 		else:
 			active_s = int(self.ACTIVE_SET)
-		if (active_s > 100):
+		if active_s > 100:
 			it = active_s // 100
 			diff = active_s
 			for i in range(it):
 				active_vals += self.chain.rpcCall('validators', [bh, str(i + 1), "100"])['validators']
 				diff -= 100
-			if (diff > 0):
+			if diff > 0:
 				active_vals += self.chain.rpcCall('validators', [bh, str(i + 2), "100"])['validators']
 		else:
 			active_vals += self.chain.rpcCall('validators', [bh, "1", str(active_s)])['validators']
@@ -121,8 +123,8 @@ class TaskTendermintHealthError(Task):
 		try:
 			self.chain.getHealth()
 			return False
-		except Exception as e:
-			return self.notify('health error! %s' % Emoji.Health)
+		except Exception as _:
+			return self.notify(f'health error! {Emoji.Health}')
 
 
 
@@ -132,7 +134,7 @@ class Tendermint (Chain):
 	BLOCKTIME = 60
 	EP = "http://localhost:26657/"
 	CUSTOM_TASKS = [TaskTendermintBlockMissed, TaskTendermintPositionChanged, TaskTendermintHealthError]
-	
+
 	def __init__(self, conf):
 		super().__init__(conf)
 
@@ -182,11 +184,11 @@ class Tendermint (Chain):
 
 	def isSynching(self):
 		return self.rpcCall('status')['sync_info']['catching_up']
-	
+
 	def getLatestProposal(self):
 		serv = self.conf.getOrDefault('chain.service')
 		if serv:
-			cmd = configparser.ConfigParser().read(f"/etc/systemd/system/{serv}")
+			cmd = configparser.ConfigParser().read(f"/etc/systemd/system/{serv}") 
 			cmd = re.split(' ', cmd["Service"]["ExecStart"])[0]
 			return json.loads(Bash(cmd+" q gov proposal --reverse --limit 1 --output json").value())["proposals"][0]
 		raise Exception('No service file name specified!')
