2e2a7940b69ef7870e5f15a5ad54b1b3e0122084
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-07-07 11:05:21 +0200

    fix elapsed for hours in chainstuck, add come back online notification

--- srvcheck/tasks/taskchainstuck.py
+++ srvcheck/tasks/taskchainstuck.py
@@ -2,6 +2,16 @@ import time
 from ..notification import Emoji
 from . import Task, minutes
 
+def elapsedToString(since):
+	elapsed = (time.time() - since)
+	if (elapsed / 60) > 60:
+		elapsed = str(int(elapsed / 60 / 60)) + ' hours and ' + str(int(elapsed / 60 % 60)) + ' minutes'
+	elif (elapsed / 60) >= 1:
+		elapsed = str(int(elapsed / 60)) + ' minutes'
+	else:
+		elapsed = str(int(elapsed)) + ' seconds'
+	return elapsed
+
 class TaskChainStuck(Task):
 	def __init__(self, conf, notification, system, chain):
 		super().__init__('TaskChainStuck', conf, notification, system, chain, chain.BLOCKTIME * 2, minutes(5))
@@ -29,12 +39,13 @@ class TaskChainStuck(Task):
 
 		if bh == self.prev:
 			self.oc += 1
-			elapsed = (time.time() - self.since)
-			if (elapsed / 60) >= 1:
-				elapsed = str(int(elapsed / 60)) + ' minutes'
-			else:
-				elapsed = str(int(elapsed)) + ' seconds'
+			elapsed = elapsedToString(self.since)
 			return self.notify(f'chain is stuck at block {bh} since {elapsed} ({self.oc}) {Emoji.Stuck}')
+
+
+		if self.oc > 1:
+			elapsed = elapsedToString(self.since)
+			self.notify (f'chain come back in sync after {elapsed} ({self.oc}) {Emoji.SyncOk}')
 
 		self.prev = bh
 		self.since = time.time()
