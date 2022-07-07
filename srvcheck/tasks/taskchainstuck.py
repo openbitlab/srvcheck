264b4fae1fb4d2214f308b5716058e8fcf0356e1
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-07-07 10:32:12 +0200

    add time info on chainstuck

--- srvcheck/tasks/taskchainstuck.py
+++ srvcheck/tasks/taskchainstuck.py
@@ -1,3 +1,4 @@
+import time
 from ..notification import Emoji
 from . import Task, minutes
 
@@ -5,6 +6,8 @@ class TaskChainStuck(Task):
 	def __init__(self, conf, notification, system, chain):
 		super().__init__('TaskChainStuck', conf, notification, system, chain, chain.BLOCKTIME * 2, minutes(5))
 		self.prev = None
+		self.since = None
+		self.oc = 0
 
 		try:
 			self.chain.getBlockHash()
@@ -21,10 +24,19 @@ class TaskChainStuck(Task):
 
 		if self.prev is None:
 			self.prev = bh
+			self.since = time.time()
 			return False
 
 		if bh == self.prev:
-			return self.notify(f'chain is stuck at block {bh} {Emoji.Stuck}')
+			self.oc += 1
+			elapsed = (time.time() - self.since)
+			if (elapsed / 60) >= 1:
+				elapsed = str(int(elapsed / 60)) + ' minutes'
+			else:
+				elapsed = str(int(elapsed)) + ' seconds'
+			return self.notify(f'chain is stuck at block {bh} since {elapsed} ({self.oc}) {Emoji.Stuck}')
 
 		self.prev = bh
+		self.since = time.time()
+		self.oc = 0
 		return False
