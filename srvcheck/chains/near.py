2317b88b92cbc35f5f071236b7c4a0667d11fadf
AlessandroPaparella <61472307+AlessandroPaparella@users.noreply.github.com>
2022-07-21 13:44:13 +0200

    check on proposals

--- srvcheck/chains/near.py
+++ srvcheck/chains/near.py
@@ -1,28 +1,29 @@
 import json
+from typing import Literal
 from ..notification import Emoji
 from .chain import Chain
 from ..tasks import Task, seconds, hours, minutes
+from ..utils import Bash
 
 class TaskNearBlockMissed (Task):
-    def __init__(self, conf, notification, system, chain, checkEvery=minutes(2), notifyEvery=minutes(5)):
-        super().__init__("TaskNearBlockMissed", conf, notification, system, chain, checkEvery, notifyEvery)
-
-        self.THRESHOLD_RATIO = 0.5
+	def __init__(self, conf, notification, system, chain, checkEvery=minutes(2), notifyEvery=minutes(5)):
+		super().__init__("TaskNearBlockMissed", conf, notification, system, chain, checkEvery, notifyEvery)
+		self.THRESHOLD_RATIO = 0.5
     
-    @staticmethod
-    def isPluggable(conf, chain):
-        return True
-
-    def run(self):
-        try:
-            r = self.chain.getProductionReport()
-            expected = int(r['num_expected_blocks'])
-            produced = int(r['num_produced_blocks'])
-            if produced/expected < self.THRESHOLD_RATIO:
-                return self.notify(f'produced only {produced} / {expected} blocks {Emoji.BlockMiss}')
-        except:
-            return False
-        return False
+	@staticmethod
+	def isPluggable(conf, chain):
+		return True
+		
+	def run(self):
+		try:
+			r = self.chain.getProductionReport()
+			expected = int(r['num_expected_blocks'])
+			produced = int(r['num_produced_blocks'])
+			if produced/expected < self.THRESHOLD_RATIO:
+				return self.notify(f'produced only {produced} / {expected} blocks {Emoji.BlockMiss}')
+		except:
+			return False
+		return False
 
 class TaskNearChunksMissed (Task):
     def __init__(self, conf, notification, system, chain, checkEvery=minutes(1), notifyEvery=minutes(3)):
@@ -45,14 +46,33 @@ class TaskNearChunksMissed (Task):
             return False
         return False
 
-    
+class TaskCheckProposal (Task):
+	def __init__(self, conf, notification, system, chain, checkEvery=hours(2), notifyEvery=hours(1)):
+		super().__init__("TaskCheckProposal", conf, notification, system, chain, checkEvery, notifyEvery)
+		self.prev_epoch = None
+	
+	@staticmethod
+	def isPuggable(conf, chain):
+		return True
+
+	def run(self):
+		if self.prev_epoch is None:
+			self.prev_epoch = self.chain.getEpoch()
+		elif self.prev_epoch != self.chain.getEpoch():
+			self.prev_epoch = self.chain.getEpoch()
+			p = self.chain.getProposal()
+			if len(p) == 0:
+				return self.notify(f'failed to send proposal {Emoji.Health}')
+			elif "Declined" in p:
+				return self.notify(f'proposal has been rejected {Emoji.LowBal}')
+		return False   
 
 class Near (Chain):
     TYPE = "near"
     NAME = ""
     BLOCKTIME = 60
     EP = "http://localhost:3030/"
-    CUSTOM_TASKS = [ TaskNearBlockMissed, TaskNearChunksMissed]
+    CUSTOM_TASKS = [ TaskNearBlockMissed, TaskNearChunksMissed, TaskCheckProposal]
 
     @staticmethod
     def detect(conf):
@@ -78,11 +98,14 @@ class Near (Chain):
         return self.rpcCall('status')['validator_account_id']
 
     def getProductionReport(self):
-        validators = self.rpcCall('validators')['current_validators']
-        for v in validators:
-            if v['account_id'] == self.getPoolId():
-                return v
-        raise Exception('Node is not in the current validators!')
+		validators = self.rpcCall('validators')['current_validators']
+		for v in validators:
+			if v['account_id'] == self.getPoolId():
+				return v
+		raise Exception('Node is not in the current validators!')
 
-    
+	def getEpoch(self):
+		return int(self.rpcCall("block", { "finality": "final" })['header']['epoch_id'])
 
+	def getProposal(self):
+		return Bash(f"near proposals | grep {self.getPoolId()}").value()
