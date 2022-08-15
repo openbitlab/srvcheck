27485561ebe2311249e28f3865d77e73a9f14df6
Lu191 <gigiamendolagine.la@gmail.com>
2022-08-15 10:57:49 +0200

    Added test case

--- tests/task_tendermint_test.py
+++ tests/task_tendermint_test.py
@@ -32,10 +32,20 @@ class TestTaskTendermintNewProposal(unittest.TestCase):
 		print(n.events)
 		self.assertEqual(len(n.events), 0)
 
-	def test_alert(self):
+	def test_noalert_no_voting_period(self):
 		c, n, t, s = buildTaskEnv(TaskTendermintNewProposal, MockChainTendermint)
 		t.prev = [{"id":"2","content":{"@type":"/ibc.core.client.v1.ClientUpdateProposal","title":"upgrade client","description":"upgrade light client","subject_client_id":"07-tendermint-0","substitute_client_id":"07-tendermint-2"},
 		"status":"PROPOSAL_STATUS_FAILED","final_tally_result":{"yes":"103022468704","abstain":"56100000","no":"115992000","no_with_veto":"90000000"},"submit_time":"2022-04-25T06:22:25.564973762Z","deposit_end_time":"2022-05-09T06:22:25.564973762Z","total_deposit":[{"denom":"ufis","amount":"1000000000"}],
+		"voting_start_time":"2022-04-25T06:23:01.163682205Z","voting_end_time":"2022-04-26T06:23:01.163682205Z"}]		
+		t.run()
+		n.flush()
+		print(n.events)
+		self.assertEqual(len(n.events), 0)
+
+	def test_alert(self):
+		c, n, t, s = buildTaskEnv(TaskTendermintNewProposal, MockChainTendermint)
+		t.prev = [{"id":"2","content":{"@type":"/ibc.core.client.v1.ClientUpdateProposal","title":"upgrade client","description":"upgrade light client","subject_client_id":"07-tendermint-0","substitute_client_id":"07-tendermint-2"},
+		"status":"PROPOSAL_STATUS_VOTING_PERIOD","final_tally_result":{"yes":"103022468704","abstain":"56100000","no":"115992000","no_with_veto":"90000000"},"submit_time":"2022-04-25T06:22:25.564973762Z","deposit_end_time":"2022-05-09T06:22:25.564973762Z","total_deposit":[{"denom":"ufis","amount":"1000000000"}],
 		"voting_start_time":"2022-04-25T06:23:01.163682205Z","voting_end_time":"2022-04-26T06:23:01.163682205Z"}]
 		t.run()
 		n.flush()
