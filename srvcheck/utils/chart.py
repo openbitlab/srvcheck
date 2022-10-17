6c8482d275300763d744804ccbbe4531c6fceb05
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-10-17 11:55:06 +0200

    fix

--- srvcheck/utils/chart.py
+++ srvcheck/utils/chart.py
@@ -2,13 +2,13 @@ import dateutil.parser
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def savePlot(name, data, label, f):
+def savePlot(name, data, label, f, dataMod: lambda y: y):
 	plt.clf()
 	fig, ax1 = plt.subplots()
 
 	#ax2 = ax1.twinx()
 	ax1.title.set_text(name)
-	ax1.plot(list(map(lambda l: dateutil.parser.isoparse(l[0]), data)), list(map(lambda l: l[1], data)), 'g-')
+	ax1.plot(list(map(lambda l: dateutil.parser.isoparse(l[0]), data)), list(map(lambda l: dataMod(l[1]), data)), 'g-')
 	# ax2.plot(list(map(lambda l: dateutil.parser.isoparse(l[0]), data)), list(map(lambda l: l['s'] if 's' in l else 0, self.borderHistory)), 'b-')
 	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
 	plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
