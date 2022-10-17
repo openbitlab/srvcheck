651970115f95021e97734781a0f71033f9a5d02e
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-10-17 11:43:14 +0200

    add disk usage plotting

--- srvcheck/tasks/tasksystemusage.py
+++ srvcheck/tasks/tasksystemusage.py
@@ -1,4 +1,5 @@
 from . import Task, hours
+from ..utils import savePlot
 
 class TaskSystemUsage(Task):
 	def __init__(self, services):
@@ -16,5 +17,9 @@ class TaskSystemUsage(Task):
 		for x in ['diskUsed', 'diskPercentageUsed', 'diskUsedByLog', 'ramUsed']:
 			if self.s.persistent.hasPassedNHoursSinceLast(self.name + '_' + x, 23):
 				self.s.persistent.timedAdd(self.name + '_' + x, usage[x])
+
+
+		savePlot("Disk Percentage Used", self.s.persistent.get(self.name + '_' + 'diskPercentageUsed'), '%% used', '/tmp/t.png')
+		self.s.notification.sendPhoto('/tmp/t.png')
 
 		return False
