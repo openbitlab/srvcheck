b05590ec2482a3d39417a52b4bc72ef3105c293f
Davide Gessa (dakk) <gessadavide@gmail.com>
2022-10-20 09:21:05 +0200

    fix tasksystemusage chart

--- srvcheck/tasks/tasksystemusage.py
+++ srvcheck/tasks/tasksystemusage.py
@@ -24,50 +24,57 @@ class TaskSystemUsage(Task):
 		# savePlot("Disk Percentage Used", self.s.persistent.get(self.name + '_' + 'diskPercentageUsed'), '%% used', '/tmp/t.png')
 		# self.s.notification.sendPhoto('/tmp/t.png')
 
-		pc = PlotConf ()
-		pc.name = self.s.conf.getOrDefault('chain.name') + " - Disk"
-		pc.data = self.s.persistent.get(self.name + '_diskUsed')
-		pc.label = 'Used (GB)'
-		pc.data_mod = lambda y: y/1024/1024
+		# pc = PlotConf ()
+		# pc.name = self.s.conf.getOrDefault('chain.name') + " - Disk"
+		# pc.data = self.s.persistent.get(self.name + '_diskUsed')
+		# pc.label = 'Used (GB)'
+		# pc.data_mod = lambda y: y/1024/1024
 
 
-		pc.data2 = self.s.persistent.get(self.name + '_diskPercentageUsed')
-		pc.label2 = 'Used (%)'
-		pc.data_mod2 = lambda y: y
+		# pc.data2 = self.s.persistent.get(self.name + '_diskPercentageUsed')
+		# pc.label2 = 'Used (%)'
+		# pc.data_mod2 = lambda y: y
 
-		pc.fpath = '/tmp/t.png'
+		# pc.fpath = '/tmp/t.png'
 
-		savePlot(pc)
-		self.s.notification.sendPhoto('/tmp/t.png')
-
+		# savePlot(pc)
+		# self.s.notification.sendPhoto('/tmp/t.png')
 
 
 		pc = PlotsConf ()
 		pc.title = self.s.conf.getOrDefault('chain.name') + " - System usage"
 
 		sp = SubPlotConf()
-		sp.name = self.s.conf.getOrDefault('chain.name') + " - Disk"
+		# sp.name = self.s.conf.getOrDefault('chain.name') + " - Disk"
 		sp.data = self.s.persistent.get(self.name + '_diskUsed')
 		sp.label = 'Used (GB)'
 		sp.data_mod = lambda y: toGB(y)
+		sp.color = 'b'
+
+		# sp.data2 = data['TaskSystemUsage_diskPercentageUsed'][-14::]
+		# sp.label2 = 'Used (%)'
+		# sp.data_mod2 = lambda y: y
 		pc.subplots.append(sp)
 
 		sp = SubPlotConf()
 		sp.data = self.s.persistent.get(self.name + '_diskPercentageUsed')
 		sp.label = 'Used (%)'
 		sp.data_mod = lambda y: y
+		sp.color = 'r'
 		pc.subplots.append(sp)
 
 		sp = SubPlotConf()
 		sp.data = self.s.persistent.get(self.name + '_diskUsedByLog')
 		sp.label = 'Used by log (GB)'
 		sp.data_mod = lambda y: toGB(y)
+		sp.color = 'g'
 		pc.subplots.append(sp)
 
 		sp = SubPlotConf()
 		sp.data = self.s.persistent.get(self.name + '_ramUsed')
 		sp.label = 'Ram used (GB)'
 		sp.data_mod = lambda y: toGB(y)
+		sp.color = 'y'
 		pc.subplots.append(sp)
 
 
