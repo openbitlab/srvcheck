038605acd2e0d87cf97daad3412013ab57300f33
Lu191 <gigiamendolagine.la@gmail.com>
2022-11-22 17:23:41 +0100

    Fix date format

--- srvcheck/utils/chart.py
+++ srvcheck/utils/chart.py
@@ -2,7 +2,6 @@ import dateutil.parser
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-
 class PlotConf:
 	def __init__(self):
 		self.name = ""
@@ -27,15 +26,13 @@ def savePlot(c):
 	ax1.plot(list(map(lambda l: (dateutil.parser.isoparse(l[0])), c.data)), list(map(lambda l: (c.data_mod(l[1])), c.data)), 'g-')
 	if c.data2:
 		ax2.plot(list(map(lambda l: (dateutil.parser.isoparse(l[0])), c.data2)), list(map(lambda l: (c.data_mod2(l[1])), c.data2)), 'b-')
-	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
-	plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
 	ax1.set_xlabel('Date')
 	ax1.set_ylabel(c.label, color='g')
 	if c.data2:
 		ax2.set_ylabel(c.label2, color='b')
 
 	plt.savefig(c.fpath, bbox_inches="tight")
-
 
 class SubPlotConf:
 	def __init__(self):
