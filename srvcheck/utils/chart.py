84932b90d4448fdb4cf81527db26bbd1f59e7105
Lu191 <gigiamendolagine.la@gmail.com>
2023-02-11 13:47:39 +0100

    Fix color palettes

--- srvcheck/utils/chart.py
+++ srvcheck/utils/chart.py
@@ -78,16 +78,16 @@ def savePlots(c, s1, s2):
 		ax.plot(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data)),
 			list(map(lambda l: (sp.data_mod(l[1])), sp.data)), '-' + sp.color)
 		ax.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data)),
-			list(map(lambda l: (sp.data_mod(l[1])), sp.data)), facecolor=setColor(sp.color), interpolate=True)
+			list(map(lambda l: (sp.data_mod(l[1])), sp.data)), facecolor=setColor(sp.color), interpolate=True, alpha=.3)
 
 		if sp.data2 and not sp.share_y:
 			ax2.plot(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data2)), list(map(lambda l: (sp.data_mod2(l[1])), sp.data2)), sp.color2 + '-')
-			ax2.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data2)), list(map(lambda l: (sp.data_mod2(l[1])), sp.data2)), facecolor=setColor(sp.color2), interpolate=True)
+			ax2.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data2)), list(map(lambda l: (sp.data_mod2(l[1])), sp.data2)), facecolor=setColor(sp.color2), interpolate=True, alpha=.3)
 			ax2.set_ylabel(sp.label2, color=sp.color2)
 		elif sp.data2 and sp.share_y:
 			ax.plot(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data2)), list(map(lambda l: (sp.data_mod2(l[1])), sp.data2)), sp.color2 + '-')
-			ax.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data2)), list(map(lambda l: (sp.data_mod2(l[1])), sp.data2)), facecolor=setColor(sp.color2), interpolate=True)
-			ax.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data)), list(map(lambda l: (sp.data_mod(l[1])), sp.data)), facecolor=setColor(sp.color), interpolate=True)
+			ax.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data2)), list(map(lambda l: (sp.data_mod2(l[1])), sp.data2)), facecolor=setColor(sp.color2), interpolate=True, alpha=.8)
+			ax.fill_between(list(map(lambda l: (dateutil.parser.isoparse(l[0])), sp.data)), list(map(lambda l: (sp.data_mod(l[1])), sp.data)), facecolor=setColor(sp.color), interpolate=True, alpha=.8)
 
 		# ax.set_xlabel('Date')
 		ax.set_ylabel(sp.label, color=sp.color)
@@ -101,12 +101,12 @@ def savePlots(c, s1, s2):
 
 def setColor(color):
 	if color == 'b':
-		return 'blue'
+		return '#58508d'
 	elif color == 'r':
-		return 'red'
+		return '#ff6361'
 	elif color == 'y':
-		return 'yellow'
+		return '#ffa600'
 	elif color == 'g':
-		return 'green'
+		return '#558f4c'
 	else:
 		return 'none'
