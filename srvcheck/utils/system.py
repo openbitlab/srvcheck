419fbb54877af2ad81474ef6e08efb75dbf8358a
Davide Gessa (dakk) <gessadavide@gmail.com>
2023-08-12 13:42:27 +0200

    fix for /var/log size query

--- srvcheck/utils/system.py
+++ srvcheck/utils/system.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 
 import psutil
 import requests
@@ -25,6 +26,20 @@ def toPrettySize(size):
         return "%.1f GB" % (v / 1024.0)
     else:
         return "%d MB" % (int(v))
+
+
+def get_directory_size(directory_path):
+    total_size = 0
+    try:
+        for file in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, file)
+            if os.path.isfile(file_path):
+                total_size += os.path.getsize(file_path)
+            elif os.path.isdir(file_path):
+                total_size += get_directory_size(file_path)
+    except PermissionError as _:  # noqa: F841
+        pass
+    return total_size
 
 
 class SystemUsage:
@@ -93,7 +108,7 @@ class System:
         u.diskSize = dd.total
         u.diskUsed = dd.used
         u.diskPercentageUsed = dd.percent
-        u.diskUsedByLog = psutil.disk_usage("/var/log/").used
+        u.diskUsedByLog = get_directory_size("/var/log")
 
         mem = psutil.virtual_memory()
         u.ramSize = mem.total
