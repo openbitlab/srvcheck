858b0137a7c2114f9c63a24208daf8d13b69ee0e
alessandropaparella <alexpapar@hotmail.com>
2023-08-30 19:36:17 +0200

    add task

--- srvcheck/main.py
+++ srvcheck/main.py
@@ -35,6 +35,7 @@ ConfSet.addItem(
 )
 ConfSet.addItem(ConfItem("chain.service", None, str, "node service name"))
 ConfSet.addItem(ConfItem("tasks.govAdmin", None, str, "Proposal voter nickname"))
+ConfSet.addItem(ConfItem("tasks.horcruxService", None, str, "Horcrux service name"))
 
 
 def addTasks(services):
