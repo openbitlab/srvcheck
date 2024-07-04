97947f87bfe8ee8b914890632fc036ee3ddf5aee
Davide Gessa (dakk) <gessadavide@gmail.com>
2024-07-04 12:59:40 +0200

    remove emoji from start pitn

--- srvcheck/main.py
+++ srvcheck/main.py
@@ -115,7 +115,7 @@ def main():  # noqa: C901
         ):
             notification.addProvider(v(conf))
 
-    print(f"starting monitor v{version} {Emoji.Start}")
+    print(f"starting monitor v{version}")
     system = System(conf)
     print(system.getUsage())
 
